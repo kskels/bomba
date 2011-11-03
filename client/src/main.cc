7f149d5163b5f07cb6e6701369e81cd802bf0029
Mathias <ughadunk@gmail.com>
2011-11-02 21:59:58 -0500

    mathisssss

--- client/src/main.cc
+++ client/src/main.cc
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <irrlicht/irrlicht.h>
+#include <memory>
+
+using namespace irr;
+
+const int WIDTH = 320;
+const int HEIGHT = 240;
 
 int main() {
-  std::cout << "\\o/" << std::endl;
+  std::cout << "\\Weeeeee/" << std::endl;
+
+  SIrrlichtCreationParameters params = SIrrlichtCreationParameters();
+  params.AntiAlias = true;
+  params.WithAlphaChannel = true;
+  params.DeviceType = EIDT_BEST;
+  params.DriverType = video::EDT_OPENGL;
+  params.WindowSize = core::dimension2d<u32>(WIDTH,HEIGHT);
+  params.Doublebuffer = true;
+  params.Bits = 32;
+  params.ZBufferBits = 16;
+  params.Fullscreen = false;
+  params.Vsync = params.Fullscreen;
+  std::auto_ptr<IrrlichtDevice> device(createDeviceEx(params));
+  
+  std::wstring capt = L"Bomba - ver: 0.0.1b ";
+  device->setWindowCaption(capt.c_str());
+
+  while(device->run()) {
+  }
+
+  device.release()->drop();
+
 }
