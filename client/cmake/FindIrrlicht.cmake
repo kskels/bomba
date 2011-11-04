c89ba598379b25d3621a8339c2120a04c710ed80
Kaspars <kaspars.skels@gmail.com>
2011-11-04 21:27:33 +0100

    Added target for bomba_protobuf lib

--- client/cmake/FindIrrlicht.cmake
+++ client/cmake/FindIrrlicht.cmake
@@ -2,18 +2,18 @@
 UNSET(IRRLICHT_INC_DIR CACHE)
 FIND_PATH(IRRLICHT_INC_DIR irrlicht/irrlicht.h 
     HINTS /usr/include /usr/local/include $ENV{IRRLICHT_ROOT}/include
-)
+  )
 
 UNSET(IRRLICHT_LIB CACHE)
 FIND_LIBRARY(IRRLICHT_LIB Irrlicht
-	HINTS /usr/lib /usr/local/lib $ENV{IRRLICHT_ROOT}/lib
-)
+    HINTS /usr/lib /usr/local/lib $ENV{IRRLICHT_ROOT}/lib
+  )
 
 IF(NOT IRRLICHT_INC_DIR OR NOT IRRLICHT_LIB)
-    message(SEND_ERROR "Irrlicht could not be found")
+  message(SEND_ERROR "Irrlicht could not be found")
 ELSE(NOT IRRLICHT_INC_DIR OR NOT IRRLICHT_LIB)
-    message(STATUS "Irrlicht found: 
-        ${IRRLICHT_INC_DIR}/irrlicht/irrlicht.h 
-        ${IRRLICHT_LIB}")
+  message(STATUS "Irrlicht found: 
+     ${IRRLICHT_INC_DIR}/irrlicht/irrlicht.h 
+     ${IRRLICHT_LIB}")
 ENDIF(NOT IRRLICHT_INC_DIR OR NOT IRRLICHT_LIB)
 
