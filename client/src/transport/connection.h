e509ebe0a81a0c52becf6d4950a47b39cbaf7db2
Peter Backman <ptr.bckmn@gmail.com>
2011-11-05 11:06:22 +0100

    stub for server connection

---
+++ client/src/transport/connection.h
@@ -0,0 +1,33 @@
+// -*- c++ -*-
+
+#ifndef BOMBA_CONNECTION_H
+#define BOMBA_CONNECTION_H
+
+#include <vector>
+
+class NetMsg;
+
+/**
+ * All operations are nonblocking.
+ */ 
+class Connection {
+public:
+  enum {
+	DISCONNECTED,
+	CONNECTING,
+	CONNECTED,
+  };
+	
+  void connect(const std::string &address, int port = 14242) {}
+  void disconnect() {}
+  void send(const NetMsg &) {}
+
+  /**
+   * Reads the next incoming message.
+   * @return a pointer to the NetMsg received, or 0 if
+   *         there's not enough data to construct a message.
+   */	 
+  NetMsg *receive(NetMsg &) {}
+};
+
+#endif // !BOMBA_CONNECTION_H
