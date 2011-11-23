b73944a0d94a7db2db7264115eea6573ab7677e3
Peter Backman <ptr.bckmn@gmail.com>
2011-11-23 23:52:32 +0100

    doc update

--- server/src/main/scala/ProtoBroker.scala
+++ server/src/main/scala/ProtoBroker.scala
@@ -3,6 +3,8 @@ import akka.actor.Actor
 /**
  * Starts a server and receives protobuf M type messages.
  * Don't rely on TCP features in case we need to migrate to UDP.
+ * The server receives messages and forwards them to the session's actor. If
+ * the actor dies, any session metadata is removed
  */
 class ProtoBroker[M] {
   def run[T <: Actor](implicit m: Manifest[T]) {
